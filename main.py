992ae88fe8ac52b7db264d9206ffab141e1af117
Rafael Rivas <rafaelrivas@Rafaels-MacBook-Pro.local>
2017-08-09 09:28:46 -0700

    main.py stuff

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import webapp2
 from google.appengine.ext import ndb
 import logging
 import jinja2
+import os
 
 jinja_environment = jinja2.Environment(
    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
