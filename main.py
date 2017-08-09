94927c58518204eb12a536e5f4fef06c132e3814
Nick Tralongo <Nick@admins-Mac-Pro-9.local>
2017-08-09 16:58:37 -0700

    Sign in Change

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
 from google.appengine.ext import ndb
 import logging
@@ -23,14 +8,6 @@ import os
 jinja_environment = jinja2.Environment(
    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-# add_item_form = '''
-# <form action = "store_item" method = "post">
-#    Name: <input type = "text" name = "item_name"/><br>
-#    Quanity: <input type = "number" name = "item_quantity" min = 1/> <br>
-#    <input type = "submit"/>
-#
-# </form>
-# '''
 
 class storeItemHandler(webapp2.RequestHandler):
     def post(self):
@@ -103,5 +80,5 @@ app = webapp2.WSGIApplication([
 
     ('/store_item',storeItemHandler),
     ('/home', Home)
-    
+
 ], debug=True)
