4b8fbe3325392fee308e91e697f4e125a12f3102
Rafael Rivas <rafaelrivas@Rafaels-MacBook-Pro.local>
2017-08-08 19:19:17 -0700

    main.py stuff

--- main.py
+++ main.py
@@ -15,38 +15,61 @@
 # limitations under the License.
 #
 import webapp2
-from database import User
+from google.appengine.ext import ndb
+import logging
+add_item_from = '''
+<form action = "store_item" method = "post">
+    Name: <input type = "text" name = "item_name"/><br>
+    Quanity: <input type = "number" name = "item_quantity" min = 1/> <br>
+    <input type = "submit"/>
+
+</form>
+
+'''
 
 
+class storeItemHandler(webapp2.RequestHandler):
+    def post(self):
+        item_name = self.request.get('item_name')
+        item_quantity = int(self.request.get('item_quantity'))
+
+        existing_item_query = ShoppingItem.query(ShoppingItem. name == item_name)
+        existing_item = existing_item_query.get()
+        if existing_item is not None:
+            if item_quantity == 0:
+                existing_item.key.delete()
+            else:
+
+                existing_item.quantity += item_quantity
+                existing_item.put()
+        else:
+            if item_quantity > 0:
+                item = ShoppingItem(name = item_name, quantity = item_quantity)
+                key = item.put()
+                logging.info('The key for the item %s is %s' % (item,key))
+        self.redirect('/')
+
+class AddItemHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(add_item_from)
 
 
+class ShoppingItem(ndb.Model):
+    name = ndb.StringProperty(required = True)
+    quantity = ndb.IntegerProperty(required = True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        if user:
-            logout_link = users.create_logout_url('/')
-            logging.info('The user is logged in')
-            user_id = user.user_id()
-            if user_id not in users_data:
-                self.response.write(register_form)
-                return
-            name = User('name')
-            age = User('age')
-            birthday = User('birthday')
-            favorite_sport = User('favorite_sport')
-
-            self.response.write('Hello %s !' %
-            (name)
-            )
-
-
-
-
-
-    def post(self):
-        player = User(name = self.request.get("name"), age=self.request.get("age"), birthday = self.request. )
-        key = student.put()
+        items_query = ShoppingItem.query()
+        items = items_query.fetch()
+        self.response.write('<ul>')
+        for item in items:
+            self.response.write('<li>%s (%d) </li>' % (item.name, item.quantity))
+        self.response.write('</ul>')
+        self.response.write('<a href = "/add_item"> Add Items</a>')
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/add_item', AddItemHandler),
+    ('/store_item',storeItemHandler)
+
 ], debug=True)
